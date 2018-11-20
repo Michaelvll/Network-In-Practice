f511124a4ddae922c3abd8a92ea7506dbdae6048
Michael Wu <wz.wzh@qq.com>
2018-11-20 22:54:08 +0800

    Delete here

--- Assignment3/progAssign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ Assignment3/progAssign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -91,8 +91,6 @@ public class Router extends Device
 		
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
-		System.out.println("here");
-
 		if (etherPacket.getEtherType() != Ethernet.TYPE_IPv4) {
 			System.out.println("Not IPv4 packet dropped");
 			return;
